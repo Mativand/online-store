38a9acc4cc4a7c6b7fcca68b95764822b63db360
mat <ivandaev9@gmail.com>
2023-06-07 23:24:39 -0500

    Configured the routing.

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,18 +2,41 @@ import {createBrowserRouter, RouterProvider} from "react-router-dom";
 import {Home} from "./home";
 import {Admin} from "./admin";
 import {ErrorPage} from "./error";
+import {ADMIN_ROUTE, AUTH_ROUTE, CART_ROUTE, PRODUCTS_ROUTE} from "../shared/utils/consts.ts";
+import {Auth} from "./auth";
+import {Products} from "./products";
+import {Cart} from "./cart";
 
-const router = createBrowserRouter([
+const routes = [
     {
         path: "/",
         element: <Home />,
         errorElement: <ErrorPage/>
     },
     {
-        path: "admin",
+        path: AUTH_ROUTE,
+        element: <Auth />,
+    },
+    {
+        path: PRODUCTS_ROUTE,
+        element: <Products />,
+    },
+    {
+        path: CART_ROUTE,
+        element: <Cart />,
+    },
+];
+
+const authRoutes = [
+    {
+        path: ADMIN_ROUTE,
         element: <Admin />,
     },
-]);
+];
+
+const isAuth = true;
+
+const router = createBrowserRouter(isAuth ? [...authRoutes, ...routes] : routes);
 
 export const Routing = () => {
     return (
