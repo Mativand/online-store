e3bed82a2490761d225c336b48d2c4016632223e
mat <ivandaev9@gmail.com>
2023-06-09 00:00:07 -0500

    Added UserStore.ts

---
+++ src/entities/user/model/UserStore.ts
@@ -0,0 +1,32 @@
+import {makeAutoObservable} from "mobx";
+
+interface IUser {
+    id: number;
+    name: string;
+}
+
+export default class UserStore {
+     _isAuth: boolean;
+     _user: IUser | null;
+    constructor() {
+        this._isAuth = false;
+        this._user = null;
+        makeAutoObservable(this)
+    }
+
+    setIsAuth(value: boolean) {
+        this._isAuth = value;
+    }
+
+    setUser(user: IUser) {
+        this._user = user;
+    }
+
+    isAuth() {
+        return this._isAuth;
+    }
+
+    user() {
+        return this._user;
+    }
+}
