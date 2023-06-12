8ebc559a7cef600b0c8edeac9b1675a1768130c6
mat <ivandaev9@gmail.com>
2023-06-11 19:41:28 -0500

    Adjusted header

--- src/widgets/header/ui/index.tsx
+++ src/widgets/header/ui/index.tsx
@@ -1,17 +1,28 @@
 import {FC, useContext} from "react";
 import {AppContext} from "@/app";
 import {observer} from "mobx-react-lite";
+import {useNavigate} from "react-router-dom";
+import {ADMIN_ROUTE, HOME_ROUTE} from "@/shared/utils/consts.ts";
 
 
 export const Header: FC = observer(() => {
     const {user} = useContext(AppContext);
+    const navigate = useNavigate();
+
+    const onLogout = () => {
+        user?.setIsAuth(false);
+        navigate(HOME_ROUTE);
+    }
+
     return (
         <>
             {
                 user?.isAuth() ?
                     <div>
-                        <button>Admin panel</button>
-                        <button onClick={() => user?.setIsAuth(false)}>
+                        <button onClick={() => navigate(ADMIN_ROUTE)}>
+                            Admin panel
+                        </button>
+                        <button onClick={onLogout}>
                             Log out
                         </button>
                     </div>
